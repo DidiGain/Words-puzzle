b2bff62ac46d78f1e0efb24bcf93cf115a64bfe9
Didi <dignara@yandex.ru>
2019-08-02 10:23:33 +0300

    Initial Commit

---
+++ Words puzzle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Words puzzle
+//
+//  Created by Di Gain on 02/08/2019.
+//  Copyright © 2019 Didi Gain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
